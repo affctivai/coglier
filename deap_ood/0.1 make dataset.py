24a0ef0f24854efe260f5495fa5129076b3ba168
DH CHOI <bluetifulc@gmail.com>
2023-06-23 21:02:14 +0900

    DEAP 관련 코드 추가

--- deap_ood/0.1 make dataset.py
+++ deap_ood/0.1 make dataset.py
@@ -75,6 +75,38 @@ def make_dataset_SD(src, sublists, label, save_folder):
     print(f'num of train: {len(Y_train)} \t num of valid: {len(Y_valid)} \t num of test: {len(Y_test)}\n')
 
 
+# High Low dataset
+def make_dataset_HL(src, ranks, cut, label, save_folder):
+    ranks = [str(sub).zfill(2) for sub in ranks]
+    # cut = int(28 * cut_rate)
+    higs = ranks[: cut]
+    lows = ranks[cut :]
+    print(label, 'high', len(higs), '명', higs)
+    print(label, 'low ', len(lows), '명', lows)
+
+    # make High dataset. train 80 : valid 10 : test 10
+    datas_h, targets_h = getFromnpz(src, higs, out=True, cla=label)
+    datas_h = make_grid(datas_h)
+
+    X_train, X, Y_train, Y = train_test_split(datas_h, targets_h, test_size=0.2, stratify=targets_h, random_state=SEED)
+    X_valid, X_test, Y_valid, Y_test = train_test_split(X, Y, test_size=0.5, stratify=Y, random_state=SEED)
+    print(f'High train: {len(Y_train)} \t High valid: {len(Y_valid)}\t')
+
+    # make Low dataset.
+    datas_l, targets_l = getFromnpz(src, lows, out=True, cla=label)
+    datas_l = make_grid(datas_l)
+    print(f'testset for measuring OOD performance| Highs: {len(Y_test)}, Lows: {len(targets_l)}')
+
+    ## save
+    os.makedirs(save_folder, exist_ok=True)
+    names = f'{split(src)[-1]}_{label}' # seg_DE_v
+
+    save_dataset(save_folder, names, 'train', X_train, Y_train)
+    save_dataset(save_folder, names, 'valid', X_valid, Y_valid)
+    save_dataset(save_folder, names, 'test', X_test, Y_test)
+
+    save_dataset(save_folder, names, 'lows', datas_l, targets_l)
+
 # -----------------------------------------main---------------------------------------------------
 CHLS = [
     'FP1', 'AF3', 'F3', 'F7', 'FC5', 'FC1', 'C3', 'T7', 'CP5', 'CP1', 'P3',
@@ -95,7 +127,7 @@ LOCATION = [['-', '-', '-', 'FP1', '-', 'FP2', '-', '-', '-'],
 # preprocessed data folder location (After 0.0 preprocessing.py)
 DATAS = join(os.getcwd(),"deap_data", 'DEAP_npz')
 DATA = join(DATAS, 'seg_DE') # segmentation, DE
-LABEL = '4' # 4, v, a
+LABEL = 'v' # 4, v, a
 # subjects ID list
 SUBLIST = [str(i).zfill(2) for i in range(1, 33)] # '01', '02', '03', ..., '28'
 
@@ -104,9 +136,17 @@ CHANNEL_LOCATION_DICT = format_channel_location_dict(CHLS, LOCATION)
 togrid = ToGrid(CHANNEL_LOCATION_DICT)
 
 
+
+
 # ---------------------------------------save data------------------------------------------------
 # Sub Independent
 make_dataset_SI(DATA, SUBLIST, LABEL, join(DATAS, 'baseline'))
 
 # Sub dependent
 make_dataset_SD(DATA, SUBLIST, LABEL, join(DATAS, 'SubDepen'))
+# After 0.2 subdepend.py
+folder_name = 'Highs'
+
+## subdepend results
+RANKS = [7,16,15,23,9,1,10,32,3,27,18,29,30,6,2,19,31,8,17,20,14,13,5,4,26,24,21,22,28,11,25,12]
+make_dataset_HL(DATA, RANKS, cut=32-8, label='v', save_folder=join(DATAS, folder_name))
