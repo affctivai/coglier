d0cdd4926059dd963eb3163e7d218e7d21d06c8c
HyoSeonChoi <13eye42@naver.com>
2023-07-06 08:09:57 +0900

    반영

--- GAMEEMO_OOD/0.2 subdepend.py
+++ GAMEEMO_OOD/0.2 subdepend.py
@@ -7,7 +7,8 @@ import subprocess
 def run(sublist):
     for sub in sublist:
         print(sub)
-        subprocess.run(f'python subdepend.py --subID={sub} --batch={BATCH} --epoch={EPOCH} --target={lb} --project_name={project_name}')
+        subprocess.run(f'python subdepend.py --subID={sub} --batch={BATCH} --epoch={EPOCH} --target={LABEL}\
+         --project_name={project_name} --dname={DNAME}')
 
 def save_results(sublist):
     test_results = dict()
@@ -19,21 +20,21 @@ def save_results(sublist):
     df = pd.DataFrame.from_dict(test_results, orient='index')
     df.to_excel(join(projcet_path, f'{train_name}_results.xlsx'))
 
-
-project_name = 'Subdepend'
-projcet_path = join(os.getcwd(), 'results', project_name)
-
-lb = 'v'
-if lb == 'a': train_name = 'arousal'
-elif lb == 'v': train_name = 'valence'
-
+# main-----------------------------------------------------------------------------------------
+project_name = 'subdepend_de'
+# project_name = 'subdepend_psd'
+projcet_path = join(os.getcwd(), 'results', project_name+'_CCNN')
+DNAME = 'seg_DE'
+# DNAME = 'seg_PSD'
+LABEL = 'v'
+if LABEL == 'a': train_name = 'arousal'
+elif LABEL == 'v': train_name = 'valence'
 SUBLIST = [str(i).zfill(2) for i in range(1, 29)] # '01', '02', '03', ..., '28'
-
 BATCH = 64
 EPOCH = 120
 
 # run(SUBLIST)
-# save_results(SUBLIST)
+save_results(SUBLIST)
+# run(['26'])
 
-# run(['28']) # If there are subjects with errors, run again.
-
+## run(['28']) # If there are subjects with errors, run again.
