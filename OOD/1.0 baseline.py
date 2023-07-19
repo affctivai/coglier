565b8731db4c7c66a5594bce24b1e7a596db58c7
HyoSeonChoi <13eye42@naver.com>
2023-07-20 07:09:51 +0900

    set_args

--- OOD/1.0 baseline.py
+++ OOD/1.0 baseline.py
@@ -9,12 +9,11 @@ import torch
 from torchsummary import summary
 from torch.utils.data import DataLoader
 import torch.optim as optim
-import torch.optim.lr_scheduler as lr_scheduler
 import torch.nn as nn
 
 from utils.scheduler import CosineAnnealingWarmUpRestarts
 from utils.dataset import PreprocessedDataset
-from utils.model import MyCCNN, TSCeption, EEGNet
+from utils.model import CCNN, TSCeption, EEGNet, DGCNN
 from utils.tools import MyScheduler, plot_scheduler, epoch_time, plot_train_result
 from utils.tools import plot_confusion_matrix, get_roc_auc_score
 from sklearn.metrics import classification_report
@@ -38,15 +37,16 @@ def get_folder(path):
                 break
         path = Path(p)
     return path
+
 #-----------------------------------------------------------------------------------------
-
 # ---- GAMEEMO
-# DATASET_NAME = "GAMEEMO"
-# DATAS = join("C:\\", "Users", "LAPTOP", "jupydir", "DATAS", 'GAMEEMO_npz', 'Projects')
-# # LABEL = 'v' # 4, v, a
-# EPOCH = 200
-# BATCH = 64
-
+DATASET_NAME = "GAMEEMO"
+DATAS = join("C:\\", "Users", "LAPTOP", "jupydir", "DATAS", 'GAMEEMO_npz', 'Projects')
+LABEL = 'v'     # 4, v, a
+PROJECT = 'baseline'
+MODEL_NAME = 'DGCNN'    # 'CCNN', 'TSC', 'EEGNet', 'DGCNN'
+FEATURE = 'PSD'          # 'DE', 'PSD'
+BATCH = 64
 
 # ---- DEAP
 # DATASET_NAME = "DEAP"
@@ -56,11 +56,11 @@ def get_folder(path):
 # BATCH = 64
 
 # ---- SEED_IV
-DATASET_NAME = "SEED_IV"
-DATAS = join(os.getcwd(),"datasets", DATASET_NAME, "npz", "Projects")
-LABEL = '4' # 4, v, a
-EPOCH = 100
-BATCH = 128
+# DATASET_NAME = "SEED_IV"
+# DATAS = join(os.getcwd(),"datasets", DATASET_NAME, "npz", "Projects")
+# LABEL = '4' # 4, v, a
+# EPOCH = 100
+# BATCH = 128
 
 # ---- SEED
 # DATASET_NAME = "SEED"
@@ -69,22 +69,32 @@ BATCH = 128
 # EPOCH = 1
 # BATCH = 128
 
-# project_name = 'baseline_TSC'
-project_name = 'baseline_EEGNet'
-# project_name = 'baseline_de'
+#-----------------------------------------------------------------------------------------
+def set_args(project, model_name, feature, label): # 0.1 make dataset과 호환맞춘다면 편의대로...
+    if model_name == 'CCNN':
+        project_data = '_'.join([project, feature, 'grid'])
+        project_name = '_'.join([project, model_name, feature])
 
-DATA = join(DATAS, project_name)
-DNAME = 'seg'
-NAME = f'{DNAME}_{LABEL}'
-if LABEL == 'a': train_name = 'arousal'
-elif LABEL == 'v': train_name = 'valence'
-else: train_name = 'emotion'
+    elif model_name in ['TSC', 'EEGNet']:
+        project_data = '_'.join([project, 'raw'])
+        project_name = '_'.join([project, model_name])
 
-# save result in train_path
-train_path = Path(join(os.getcwd(), 'results', DATASET_NAME, project_name, train_name))
-train_path = get_folder(train_path)
+    elif model_name == 'DGCNN':
+        project_data = '_'.join([project, feature])
+        project_name = '_'.join([project, model_name, feature])
 
-def run_train():
+    if label == 'a':    train_name = 'arousal'
+    elif label == 'v':  train_name = 'valence'
+    else:               train_name = 'emotion'
+
+    data_dir = join(DATAS, project_data)
+    data_name = f'{LABEL}'
+    return data_dir, data_name, project_name, train_name
+
+DATA, NAME, project_name, train_name = set_args(PROJECT, MODEL_NAME, FEATURE, LABEL)
+
+#--------------------------------------train-------------------------------------------------------
+def run_train(model_name):
     train_path.mkdir(parents=True, exist_ok=True)
 
     # Load train, valid
@@ -92,28 +102,47 @@ def run_train():
     validset = PreprocessedDataset(DATA, NAME, 'valid')
     print(f'trainset: {trainset.x.shape} \t validset: {validset.x.shape}')
 
+    trainloader = DataLoader(trainset, batch_size=BATCH, shuffle=True)
+    validloader = DataLoader(validset, batch_size=BATCH, shuffle=False)
+
     labels_name = np.unique(validset.y) + 1
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     # Model
-    # model = MyCCNN(in_channels=trainset.x.shape[1], num_classes=len(labels_name))
-    # model = TSCeption(num_electrodes=trainset.x.shape[2], num_classes=len(labels_name))
-    model = EEGNet(num_electrodes=trainset.x.shape[2], num_classes=len(labels_name), chunk_size=256)
+    # hyperparameter 최적화 필요...
+    if model_name == 'CCNN':
+        model = CCNN(num_classes=len(labels_name))
+        max_lr = 1e-4
+        EPOCH = 100
+    elif model_name == 'TSC':
+        model = TSCeption(num_electrodes=trainset.x.shape[2], num_classes=len(labels_name), sampling_rate=128, dropout=0)
+        max_lr = 1e-3
+        EPOCH = 200
+    elif model_name == 'EEGNet':
+        model = EEGNet(chunk_size=trainset.x.shape[3], num_electrodes=trainset.x.shape[2], num_classes=len(labels_name), dropout=0)
+        max_lr = 1e-3
+        EPOCH = 200
+    elif model_name == 'DGCNN':
+        model = DGCNN(in_channels=trainset.x.shape[2], num_electrodes=trainset.x.shape[1], num_classes=len(labels_name))
+        max_lr = 1e-3
+        EPOCH = 200
 
     model = model.to(device)
     print(summary(model, trainset.x.shape[1:]))
-    # return
-
-    trainloader = DataLoader(trainset, batch_size=BATCH, shuffle=True)
-    validloader = DataLoader(validset, batch_size=BATCH, shuffle=False)
 
     STEP = len(trainloader)
     STEPS = EPOCH * STEP
 
+    optimizer = optim.Adam(model.parameters(), lr=0, weight_decay=1e-4)
+    scheduler = CosineAnnealingWarmUpRestarts(optimizer, T_0=STEPS, T_mult=1, eta_max=max_lr, T_up=STEP*3, gamma=0.5)
+
     ## plot_scheduler
     # scheduler = MyScheduler(STEP, STEPS, flag='CWR', baselr=0.0001, T_0_num= 1, T_up_num= 3)
     # plot_scheduler(scheduler.make_lrs(), save=True, path=train_path)
-    #--------------------------------------train-------------------------------------------------------
+
+    criterion = nn.CrossEntropyLoss()
+    criterion = criterion.to(device)
+
     def train(model, loader, optimizer, criterion, scheduler, device, scaler):
         epoch_loss = 0; epoch_acc = 0
         model.train()
@@ -152,23 +181,17 @@ def run_train():
                     acc = y.eq(y_pred.argmax(1)).sum() / y.shape[0]
                 epoch_loss += loss.item()
                 epoch_acc += acc.item()
-        return epoch_loss/len(loader), epoch_acc/len(loader)
-
-    criterion = nn.CrossEntropyLoss()
-    criterion = criterion.to(device)
-
-    optimizer = optim.Adam(model.parameters(), lr=0, weight_decay=1e-4)
-    scheduler = CosineAnnealingWarmUpRestarts(optimizer,T_0=STEPS,T_mult=1,eta_max=1e-3,T_up=STEP*3,gamma=0.5)
+        return epoch_loss / len(loader), epoch_acc / len(loader)
 
     lrs = []
-    train_losses, train_accs = [],[]
-    valid_losses, valid_accs = [],[]
-
+    train_losses, train_accs = [], []
+    valid_losses, valid_accs = [], []
     best_valid_loss = float('inf')
     scaler = torch.cuda.amp.GradScaler()
+    # ----------------------------------------run-------------------------------------------------------
     with open(join(train_path, 'train.txt'), 'w') as file:
         file.write(f'LABEL {LABEL}:{labels_name}\t BATCH {BATCH}\
-                    \n{DNAME}  train:{tuple(trainset.x.shape)}\tvalid:{tuple(validset.x.shape)}\
+                    \n{NAME}  train:{tuple(trainset.x.shape)}\tvalid:{tuple(validset.x.shape)}\
                     \nEpoch {EPOCH}\tTrain  Loss/Acc\tValid  Loss/Acc\n')
 
         print(f'Epoch {EPOCH}\tTrain  Loss/Acc\tValid  Loss/Acc')
@@ -197,29 +220,33 @@ def run_train():
     plot_train_result(train_losses, valid_losses, train_accs, valid_accs, EPOCH, size=(9, 5), path=train_path)
     print(f"model weights saved in '{join(train_path,'best.pt')}'")
 
-
-# run_train()
-#--------------------------------------test-------------------------------------------------------
-def run_test():
+#--------------------------------------test--------------------------------------------------------
+def run_test(model_name):
     test_path.mkdir(parents=True, exist_ok=True)
 
     # Load test
     testset = PreprocessedDataset(DATA, NAME, 'test')
     print(f'testset: {testset.x.shape}')
 
+    testloader = DataLoader(testset, batch_size=BATCH, shuffle=False)
+
     labels_name = np.unique(testset.y) + 1
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     # Model (load parameters)
-    # model = MyCCNN(in_channels=testset.x.shape[1], num_classes=len(labels_name))
-    # model = TSCeption(num_electrodes=testset.x.shape[2], num_classes=len(labels_name))
-    model = EEGNet(num_electrodes=testset.x.shape[2], num_classes=len(labels_name))
-
+    if model_name == 'CCNN':
+        model = CCNN(num_classes=len(labels_name))
+    elif model_name == 'TSC':
+        model = TSCeption(num_electrodes=testset.x.shape[2], num_classes=len(labels_name), sampling_rate=128)
+    elif model_name == 'EEGNet':
+        model = EEGNet(chunk_size=testset.x.shape[3], num_electrodes=testset.x.shape[2], num_classes=len(labels_name))
+    elif model_name == 'DGCNN':
+        model = DGCNN(in_channels=testset.x.shape[2], num_electrodes=testset.x.shape[1], num_classes=len(labels_name))
     model = model.to(device)
     model.load_state_dict(torch.load(join(train_path, 'best.pt')))
 
-    BATCH = 64
-    testloader = DataLoader(testset, batch_size=BATCH, shuffle=False)
+    criterion = nn.CrossEntropyLoss()
+    criterion = criterion.to(device)
 
     def evaluate_test(model, loader, criterion, device):
         losss, accs = [], []
@@ -228,8 +255,7 @@ def run_test():
         model.eval()
         with torch.no_grad():
             for (x, y, subID) in loader:
-                x = x.to(device)
-                y = y.to(device)
+                x = x.to(device);   y = y.to(device)
 
                 y_pred = model(x)
 
@@ -252,12 +278,11 @@ def run_test():
         subIDs = torch.cat(subIDs, dim=0)
         return np.mean(losss), np.mean(accs), labels, preds, probs, subIDs
 
-    criterion = nn.CrossEntropyLoss()
-    criterion = criterion.to(device)
-
+    # ----------------------------------------run-------------------------------------------------------
     with open(join(test_path, 'output.txt'), 'w') as file:
         file.write(f'{LABEL}:{labels_name}\t BATCH {BATCH}\
-                    \n{DNAME}  test:{tuple(testset.x.shape)}\n')
+                    \n{NAME}  test:{tuple(testset.x.shape)}\n')
+
         test_loss, test_acc, labels, preds, probs, subIDs  = evaluate_test(model, testloader, criterion, device)
         log = f'test_loss: {test_loss:.3f}\ttest_acc: {test_acc*100:6.2f}%\n'
         log += f'roc_auc_score: {get_roc_auc_score(labels, preds)}\n'
@@ -281,11 +306,13 @@ def run_test():
     plot_confusion_matrix(labels, preds, labels_name, path=test_path, lbname=train_name, title=f'{train_name} CM')
     print(f'saved in {test_path}')
 
-
-
+#---------------------------------------main-------------------------------------------------------
+# save result in train_path
 train_path = Path(join(os.getcwd(), 'results', DATASET_NAME, project_name, train_name))
-run_train()
+train_path = get_folder(train_path)
 
 test_path = Path(join(train_path), 'test')
 test_path = get_folder(test_path)
-# run_test()
+
+run_train(MODEL_NAME)
+run_test(MODEL_NAME)
